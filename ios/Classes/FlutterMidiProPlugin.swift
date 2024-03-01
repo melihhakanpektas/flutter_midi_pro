85261096251840285028a9054bca00566a85b4a0
Melih Hakan Pektas <melihhakanpektas@gmail.com>
2024-03-01 11:47:13 +0300

    testing new ios method 1

--- ios/Classes/FlutterMidiProPlugin.swift
+++ ios/Classes/FlutterMidiProPlugin.swift
@@ -5,57 +5,56 @@ import AVFoundation
 import CoreAudio
 
 public class FlutterMidiProPlugin: NSObject, FlutterPlugin {
-    private var channel: FlutterMethodChannel?
-    private var synth: AVAudioUnitMIDIInstrument?
-    private var isInitialized = false
-    private var isDisposed = false
+  var _arguments = [String: Any]()
+  var audioEngine = AVAudioEngine()
+  var samplerNode = AVAudioUnitSampler()
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_midi_pro", binaryMessenger: registrar.messenger())
+    let instance = FlutterMidiProPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
 
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_midi_pro", binaryMessenger: registrar.messenger())
-        let instance = FlutterMidiProPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-    }
-
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        let arguments = call.arguments as? [String: Any]
-        print("Method: \(call.method), Arguments: \(String(describing: arguments))")
-        if (isDisposed || !isInitialized) && (call.method != "loadSoundfont" && synth == nil) {
-            result(FlutterError(code: "NOT_INITIALIZED", message: "Synthesizer is not initialized", details: nil))
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "loadSoundfont":
+        guard let map = call.arguments as? Dictionary<String, Any>,
+              let sf2Data = map["sf2Data"] as? FlutterStandardTypedData else {
+            result(FlutterError(code: "INVALID_ARGUMENT", message: "sf2Data is required and must be a FlutterStandardTypedData", details: nil))
             return
         }
-        switch call.method {
-        case "loadSoundfont":
-            // Load soundfont code here
-            // ...
-            result("Soundfont loaded successfully")
-        case "isInitialized":
-            result(synth != nil)
-        case "changeSoundfont":
-            // Change soundfont code here
-            // ...
-            result("Soundfont changed successfully")
-        case "getInstruments":
-            // Get instruments code here
-            // ...
-            result("Instruments fetched successfully")
-        case "playMidiNote":
-            // Play MIDI note code here
-            // ...
-            result("MIDI note played successfully")
-        case "stopMidiNote":
-            // Stop MIDI note code here
-            // ...
-            result("MIDI note stopped successfully")
-        case "dispose":
-            // Dispose code here
-            // ...
-            result("Synthesizer disposed")
-        default:
-            result(FlutterMethodNotImplemented)
+
+        let sf2URL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("sounbank.sf2")
+        do {
+            try sf2Data.data.write(to: sf2URL, options: .atomic)
+        } catch {
+            result(FlutterError(code: "IO_ERROR", message: "Failed to write soundbank to file", details: error))
+            return
         }
+        do {
+            try samplerNode.loadInstrument(at: sf2URL)
+            audioEngine.attach(samplerNode)
+            audioEngine.connect(samplerNode, to: audioEngine.mainMixerNode, format: nil)
+            try audioEngine.start()
+        } catch {
+            result(FlutterError(code: "UNAVAILABLE", message: "Soundfont yüklenemedi", details: error))
+        }
+        result(nil)
+      case "play_midi_note":
+        _arguments = call.arguments as! [String : Any];
+        let note = UInt8(_arguments["note"] as! Int)
+        let velocity = UInt8(_arguments["velocity"] as! Int)
+        samplerNode.startNote(note, withVelocity: velocity, onChannel: 0)
+        let message = "Playing: \(String(describing: note))"
+        result(nil)
+      case "stop_midi_note":
+        _arguments = call.arguments as! [String : Any];
+        let note = UInt8(_arguments["note"] as! Int)
+        samplerNode.stopNote(note, onChannel: 0)
+        let message = "Stopped: \(String(describing: note))"
+        result(nil)
+    default:
+      result(FlutterMethodNotImplemented)
+        break
     }
-    
-    public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
-        channel?.setMethodCallHandler(nil)
-    }
+  }
 }
