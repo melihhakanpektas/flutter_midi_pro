8661fdc64a795f8833129276ed4cd4f43edcd742
Melih Hakan Pektas <melihhakanpektas@gmail.com>
2024-03-02 09:19:41 +0300

    macos update

--- macos/Classes/FlutterMidiProPlugin.swift
+++ macos/Classes/FlutterMidiProPlugin.swift
@@ -5,10 +5,10 @@ import AVFoundation
 import CoreAudio
 
 public class FlutterMidiProPlugin: NSObject, FlutterPlugin {
-  var message = "Please Send Message"
   var _arguments = [String: Any]()
   var audioEngine = AVAudioEngine()
   var samplerNode = AVAudioUnitSampler()
+  var tempFileURL = FileManager.default.temporaryDirectory.appendingPathComponent("temp.sf2")
   
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_midi_pro", binaryMessenger: registrar.messenger)
@@ -16,36 +16,64 @@ public class FlutterMidiProPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  public override init() {
+    audioEngine.attach(samplerNode)
+    audioEngine.connect(samplerNode, to: audioEngine.mainMixerNode, format:nil)
+    do {
+        try audioEngine.start()
+    } catch {
+        print("Error starting audio engine: \(error.localizedDescription)")
+    }
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-      case "load_soundfont":
-        let map = call.arguments as? Dictionary<String, String>
-        let data = map?["path"]
-        let url = URL(fileURLWithPath: data!)
-        do {
-          audioEngine.attach(samplerNode)
-          audioEngine.connect(samplerNode, to: audioEngine.mainMixerNode, format: nil)
-            try audioEngine.start()
-          try samplerNode.loadSoundBankInstrument(at: url, program: 0, bankMSB: UInt8(kAUSampler_DefaultMelodicBankMSB), bankLSB: UInt8(kAUSampler_DefaultBankLSB))
-          print("Valid URL: \(url)")
-        } catch {
-          print("Error preparing MIDI: \(error.localizedDescription)")
+    case "loadSoundfont":
+        guard let map = call.arguments as? Dictionary<String, Any>,
+              let sf2Data = map["sf2Data"] as? FlutterStandardTypedData else {
+            result(FlutterError(code: "INVALID_ARGUMENT", message: "sf2Data is required and must be a FlutterStandardTypedData", details: nil))
+            return
         }
-        let message = "Prepared Sound Font"
-        result(message)
-      case "play_midi_note":
+        let instrumentIndex = map["instrumentIndex"] as? Int ?? 0
+        let soundfontData = sf2Data.data as Data
+        do {
+            try soundfontData.write(to: tempFileURL, options: .atomic)
+        } catch {
+            print("Error writing temp file: \(error)")
+            return
+        }
+        do {
+              // implementing an instrument from a soundfont into a samplerNode
+            try samplerNode.loadSoundBankInstrument(at: tempFileURL, program: UInt8(instrumentIndex), bankMSB: UInt8(kAUSampler_DefaultMelodicBankMSB), bankLSB: UInt8(kAUSampler_DefaultBankLSB))
+        } catch {
+            print("Error preparing MIDI: \(error.localizedDescription)")
+        }
+        result("Soundfont changed successfully")
+    case "loadInstrument":
+        _arguments = call.arguments as! [String : Any];
+        let instrumentIndex = _arguments["instrumentIndex"] as! Int
+        do {
+              // implementing an instrument from a soundfont into a samplerNode
+            try samplerNode.loadSoundBankInstrument(at: tempFileURL, program: UInt8(instrumentIndex), bankMSB: UInt8(kAUSampler_DefaultMelodicBankMSB), bankLSB: UInt8(kAUSampler_DefaultBankLSB))
+        } catch {
+            print("Error preparing MIDI: \(error.localizedDescription)")
+        }
+        result("Instrument changed successfully")
+    case "playMidiNote":
         _arguments = call.arguments as! [String : Any];
         let note = UInt8(_arguments["note"] as! Int)
         let velocity = UInt8(_arguments["velocity"] as! Int)
         samplerNode.startNote(note, withVelocity: velocity, onChannel: 0)
-        let message = "Playing: \(String(describing: note))"
-        result(message)
-      case "stop_midi_note":
+        result(nil)
+    case "stopMidiNote":
         _arguments = call.arguments as! [String : Any];
         let note = UInt8(_arguments["note"] as! Int)
         samplerNode.stopNote(note, onChannel: 0)
-        let message = "Stopped: \(String(describing: note))"
-        result(message)
+        result(nil)
+    case "dispose":
+        audioEngine.stop()
+        audioEngine.detach(samplerNode)
+        result(nil)
     default:
       result(FlutterMethodNotImplemented)
         break
