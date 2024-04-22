45086dd8a5502b9319d7cdf678a81d70e1813db3
Melih Hakan Pektas <contact@melihhakanpektas.com>
2024-04-22 03:45:17 +0300

    deleting x86 abi because package size >100mb

--- example/android/app/src/main/kotlin/com/melihhakanpektas/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.melihhakanpektas.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/android/app/src/main/kotlin/com/melihhakanpektas/flutter_midi_pro_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.melihhakanpektas.flutter_midi_pro_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
