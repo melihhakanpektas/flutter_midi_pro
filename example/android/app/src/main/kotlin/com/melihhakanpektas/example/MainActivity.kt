086a998fcd271c9df53b3896429ce6ed27f89a1d
Melih Hakan Pektas <contact@melihhakanpektas.com>
2024-04-22 03:58:40 +0300

    Revert "deleting x86 abi because package size >100mb"
    This reverts commit 45086dd8a5502b9319d7cdf678a81d70e1813db3.

---
+++ example/android/app/src/main/kotlin/com/melihhakanpektas/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.melihhakanpektas.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_midi_pro
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMidiProPlugin.register(with: registry.registrar(forPlugin: "FlutterMidiProPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
